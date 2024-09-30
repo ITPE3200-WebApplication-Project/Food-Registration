f45d5583b508d2d673766bc7a0a7688c4fa61293
s354388 <s354388@oslomet.no>
2024-09-30 13:39:59 +0200

    mvc

--- obj/Debug/net8.0/Food-Registration.AssemblyInfo.cs
+++ obj/Debug/net8.0/Food-Registration.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Food-Registration")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36b75f0617307d088439eb936ffde335d20bd71d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3c50798e2adba3ee34190653b5f216982b827f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Food-Registration")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Food-Registration")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
