eda20019c3388a0e8506c7b5617bfb0e58e2d925
Simon Bjørnøy <simon.bjornoy@gmail.com>
2024-11-26 15:28:42 +0100

    feat: added migration

---
+++ Migrations/20241126142641_AddOwnerIds.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Food_Registration.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOwnerIds : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OwnerId",
+                table: "Products",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "OwnerId",
+                table: "Producers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "Producers");
+        }
+    }
+}
--- Models/Product.cs
+++ Models/Product.cs
@@ -4,16 +4,17 @@ namespace Food_Registration.Models
   public class Product
   {
     public int ProductId { get; set; }
+    public string OwnerId { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
     public string? Category { get; set; }
-    public decimal? Calories {get; set; }
-    public decimal? Carbohydrates {get; set; }
-    public decimal? Fat {get; set; }
-    public decimal? Protein {get; set; } 
+    public decimal? Calories { get; set; }
+    public decimal? Carbohydrates { get; set; }
+    public decimal? Fat { get; set; }
+    public decimal? Protein { get; set; }
     public string? Description { get; set; }
     public string? ImageUrl { get; set; }
 
-    public string ProducerId { get; set;} = string.Empty;
+    public string ProducerId { get; set; } = string.Empty;
 
     // public virtual Producer? Producer { get; set; } // Virtual gir lazy loading, default unngår null-warnings
   }
