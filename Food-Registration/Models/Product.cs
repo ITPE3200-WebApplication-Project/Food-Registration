896faf5ba407dd2ca3c2d7421db063ea2e4b7899
gulgulyil <72104981+gulgulyil@users.noreply.github.com>
2024-11-29 13:45:39 +0100

    Edited Product.cs

--- Food-Registration/Models/Product.cs
+++ Food-Registration/Models/Product.cs
@@ -1,4 +1,5 @@
 namespace Food_Registration.Models
+using System.ComponentModel.DataAnnotations;
 
 {
   public class Product
@@ -7,9 +8,16 @@ namespace Food_Registration.Models
     public int ProducerId { get; set; } = 0;
     public string Name { get; set; } = string.Empty;
     public string? Category { get; set; }
+    [Range(0, (double)decimal.MaxValue, ErrorMessage = "Calories cannot be negative.")]    
     public decimal? Calories { get; set; }
+    
+    [Range(0, (double)decimal.MaxValue, ErrorMessage = "Carbohydrates cannot be negative.")]
     public decimal? Carbohydrates { get; set; }
+    
+    [Range(0, (double)decimal.MaxValue, ErrorMessage = "Fat cannot be negative.")]
     public decimal? Fat { get; set; }
+    
+    [Range(0, (double)decimal.MaxValue, ErrorMessage = "Protein cannot be negative.")]
     public decimal? Protein { get; set; }
     public string? Description { get; set; }
     public string? ImageUrl { get; set; }
