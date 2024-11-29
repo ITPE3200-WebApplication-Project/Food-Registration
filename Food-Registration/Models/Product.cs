09e8a3d38af1ca0d80310e0dd2ae05a6889d9b39
gulgulyil <72104981+gulgulyil@users.noreply.github.com>
2024-11-29 16:35:38 +0100

    edited product.cs for negativ numbers

--- Food-Registration/Models/Product.cs
+++ Food-Registration/Models/Product.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 namespace Food_Registration.Models
 
 {
@@ -7,9 +8,17 @@ namespace Food_Registration.Models
     public int ProducerId { get; set; } = 0;
     public string Name { get; set; } = string.Empty;
     public string? Category { get; set; }
+
+    [Range(0, (double)decimal.MaxValue, ErrorMessage = "Calories cannot be negative.")]
     public decimal? Calories { get; set; }
+
+    [Range(0, (double)decimal.MaxValue, ErrorMessage = "Carbohydrates cannot be negative.")]
     public decimal? Carbohydrates { get; set; }
+
+    [Range(0, (double)decimal.MaxValue, ErrorMessage = "Fat cannot be negative.")]
     public decimal? Fat { get; set; }
+
+    [Range(0, (double)decimal.MaxValue, ErrorMessage = "Protein cannot be negative.")]
     public decimal? Protein { get; set; }
     public string? Description { get; set; }
     public string? ImageUrl { get; set; }
